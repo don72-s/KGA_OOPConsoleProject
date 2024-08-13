6dc62b75a1d977fca940f6cf24eca273625780dd
김민태 <66211881+don72-s@users.noreply.github.com>
2024-08-13 14:16:14 +0900

    refact: maze scene
    미궁 씬 정의

---
+++ Scenes/MazeScene.cs
@@ -0,0 +1,56 @@
+﻿using ConsoleGame.Mazes;
+using ConsoleGame.userData;
+using System.Text;
+
+namespace ConsoleGame.Scenes {
+    public class MazeScene : Scene {
+
+        Player player;
+        List<Maze> mazeArray;
+        StringBuilder sb;
+        string output;
+
+        public MazeScene(ISceneChangeable _game, IMonsterSetable _combatScene) : base(_game){
+
+            player = Player.GetInstance();
+            mazeArray =
+            [
+                new Maze(MazeBases.maze0MapBase, _game, _combatScene),
+                new Maze(MazeBases.maze1MapBase, _game, _combatScene),
+                new DarkMaze(MazeBases.maze2MapBase, _game, _combatScene),
+            ];
+
+            sb = new StringBuilder();
+            output = "";
+
+        }
+
+
+        public override void Enter() {
+        }
+
+        public override void Exit() {
+
+        }
+
+        public override void Input() {
+
+            mazeArray[player.curMazeLevel].Input(Console.ReadKey(true).Key);
+
+        }
+
+        public override void Print() {
+
+            Console.Clear();
+            mazeArray[player.curMazeLevel].Print();
+
+        }
+
+        public override void Update() {
+
+        }
+
+
+
+    }
+}
