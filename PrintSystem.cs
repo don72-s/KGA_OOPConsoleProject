63db3493a22850e060cc893d168d6623e03495f6
김민태 <66211881+don72-s@users.noreply.github.com>
2024-08-13 18:13:09 +0900

    feat: print system
    출력 전용 static 기능 개발

---
+++ PrintSystem.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleGame.Scenes;
+
+namespace ConsoleGame {
+    public static class PrintSystem {
+
+        const string clearLineStr = "                                    ";
+
+        public static void ClearLine((int, int) _pos) {
+
+            Console.SetCursorPosition(_pos.Item1, _pos.Item2);
+            Console.Write(clearLineStr);
+            Console.SetCursorPosition(_pos.Item1, _pos.Item2);
+
+        }
+
+        public static void PrintLine((int, int) _pos, string _text) {
+
+            ClearLine(_pos);
+            Console.WriteLine(_text);
+
+        }
+
+        public static void PrintLineAndWaitZInput((int, int) _pos, string _text) { 
+            
+            PrintLine(_pos, _text);
+            InputSystem.Waiting_Z_Input();
+
+        }
+
+    }
+}
