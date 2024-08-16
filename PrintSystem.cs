fb37459fc6ee19dfce821a77cb3256aae109d3a5
김민태 <66211881+don72-s@users.noreply.github.com>
2024-08-16 11:00:51 +0900

    feat: write system
    콘솔의 특정 좌표에 char, string 작성하는 system 함수 작성

--- PrintSystem.cs
+++ PrintSystem.cs
@@ -1,15 +1,52 @@
 ﻿using ConsoleGame.Scenes;
+using System.Drawing;
 
 namespace ConsoleGame {
     public static class PrintSystem {
 
         const string clearLineStr = "                                    ";
+        const char clearChar = ' ';
 
-        public static void ClearLine((int, int) _pos) {
+        public static void ClearAt((int, int) _pos) {
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            WriteAt(_pos, clearChar);
+
+        }
+        public static void ClearAt(int _posX, int _posY) {
+
+            ClearAt(new(_posX, _posY));
+
+        }
+
+        public static void WriteAt((int, int) _pos, char _char) {
+
+            (int, int) backPos = Console.GetCursorPosition();
 
             Console.SetCursorPosition(_pos.Item1, _pos.Item2);
-            Console.Write(clearLineStr);
+            Console.Write(_char);
+            Console.SetCursorPosition(backPos.Item1, backPos.Item2);
+
+        }
+        public static void WriteAt(int _posX, int _posY, char _char) {
+
+            WriteAt(new(_posX, _posY), _char);
+
+        }
+
+
+        public static void WriteAt((int, int) _pos, string _str) {
+
+            (int, int) backPos = Console.GetCursorPosition();
+
             Console.SetCursorPosition(_pos.Item1, _pos.Item2);
+            Console.Write(_str);
+            Console.SetCursorPosition(backPos.Item1, backPos.Item2);
+
+        }
+        public static void WriteAt(int _posX, int _posY, string _str) {
+
+            WriteAt(new(_posX, _posY), _str);
 
         }
 
